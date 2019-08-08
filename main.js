ceb41f971e786bc1675e4fc8a7160d9313f25d4b
Vanessa Randall <vanessalrandall@gmail.com>
2019-08-08 13:35:13 -0600

    Break functionality during a refactor attempt

--- main.js
+++ main.js
@@ -1,147 +1,149 @@
-var toDosArr 				= JSON.parse(localStorage.getItem('toDosArr')) || [];
-var addNewTask		 	= document.querySelector('.form__image--add');
-var clearBtn 				= document.querySelector('.form__button--reset');
-var nav 						= document.querySelector('.navigation');
-var newTaskSection 	= document.querySelector('.section__ul');
-var newTask 				= document.querySelector('.form__label--text');
-var submitBtn 			= document.querySelector('.form__button-submit');
-var taskTitle 			= document.querySelector('.form__label--input');
-var taskSection 		= document.querySelector('.section__tasks');
+$(document).ready(() => {
 
-nav.addEventListener('keyup', navKeyHandler);
-nav.addEventListener('click', checkEventLocation);
-taskSection.addEventListener('click', toDoTaskHandler);
-window.addEventListener('load', onPageLoad);
+	const toDosArr = JSON.parse(localStorage.getItem('toDosArr')) || [];
+	const addNewTask = $('.form__image--add');
+	const clearBtn = $('.form__button--reset');
+	const nav = $('.navigation');
+	const newTaskSection = $('.section__ul');
+	let newTask = $('.form__label--text');
+	const submitBtn = document.querySelector('.form__button-submit');
+	let taskTitle = document.querySelector('.form__label--input');
+	const taskSection = document.querySelector('.section__tasks');
+
+	nav.on('keyup', navKeyHandler);
+	nav.on('click', checkEventLocation);
+	taskSection.on('click', toDoTaskHandler);
+	window.on('load', onPageLoad);
 
 
-function onPageLoad(e) {
-	welcomeMessage();
-	persistedToDos();
-	reinstantiateToDos();
-	disableClearBtn(e);
-	disableSubmitBtn(e);
-	checkTaskInputValue(e);
-}
-
-function navKeyHandler(e) {
-	disableClearBtn(e);
-	disableSubmitBtn(e);
-	checkTaskInputValue(e);
-}
-
-function checkEventLocation(e) {
-	if (e.target.closest('.form__image--add')) {
-		checkTaskInputValue(e);
-		appendTempTask(e);
-		checkTaskInputValue(e);
-	}
-	if (e.target.closest('.form__button-submit')) {
-		createNewToDo(e);
-		clearToDoData(e);
+	function onPageLoad(e) {
+		welcomeMessage();
+		persistedToDos();
+		reinstantiateToDos();
 		disableClearBtn(e);
 		disableSubmitBtn(e);
-		welcomeMessage();
+		checkTaskInputValue(e);
 	}
-	if (e.target.closest('.section__ul')) {
-		removeTasks(e);
-	}
-	if (e.target === clearBtn) {
-		clearTasks(e);
+
+	function navKeyHandler(e) {
 		disableClearBtn(e);
+		disableSubmitBtn(e);
+		checkTaskInputValue(e);
 	}
-}
 
-function toDoTaskHandler(e) {
-	if (e.target.classList.contains('section__input--delete')) {
-		deleteToDoCard(e);
+	function checkEventLocation(e) {
+		if (e.target.closest('.form__image--add')) {
+			checkTaskInputValue(e);
+			appendTempTask(e);
+			checkTaskInputValue(e);
+		}
+		if (e.target.closest('.form__button-submit')) {
+			createNewToDo(e);
+			clearToDoData(e);
+			disableClearBtn(e);
+			disableSubmitBtn(e);
+			welcomeMessage();
+		}
+		if (e.target.closest('.section__ul')) {
+			removeTasks(e);
+		}
+		if (e.target === clearBtn) {
+			clearTasks(e);
+			disableClearBtn(e);
+		}
 	}
-	if (e.target.classList.contains('section__input--urgent')) {
-		urgentStatus(e);
+
+	function toDoTaskHandler(e) {
+		if (e.target.classList.contains('section__input--delete')) {
+			deleteToDoCard(e);
+		}
+		if (e.target.classList.contains('section__input--urgent')) {
+			urgentStatus(e);
+		}
+		if (e.target.classList.contains('li__delete')) {
+			checkedStatus(e);
+		}
 	}
-	if (e.target.classList.contains('li__delete')) {
-		checkedStatus(e);
+
+	function welcomeMessage() {
+		var welcomeText = document.querySelector('.aside__section--container');
+		if (toDosArr.length > 0) {
+			welcomeText.addClass("hidden");
+		}
+		if (toDosArr.length < 0) {
+			welcomeText.removeClass("hidden");
+		}
 	}
-}
 
-function welcomeMessage() {
-	var welcomeText = document.querySelector('.aside__section--container');
-	if (toDosArr.length > 0) {
-		welcomeText.classList.add('hidden');
+	function clearTasks(e) {
+		newTaskSection.innerHTML = "";
 	}
-	if (toDosArr.length < 0) {
-		welcomeText.classList.remove('hidden');
+
+	function removeTasks(e) {
+		if (e.target.className === 'li__delete') {
+			event.target.parentNode.remove();
+		}
 	}
-}
 
-function clearTasks(e) {
-	newTaskSection.innerHTML = "";
-}
+	function reassignClass(id, title, tasks, urgent, finished, i) {
+		let toDo = new ToDo(id, title, tasks, urgent, finished);
+		toDosArr.splice(i, 1, toDo);
+	};
 
-function removeTasks(e) {
-	if (e.target.className === 'li__delete') {
-		event.target.parentNode.remove();
+	function reassignTaskClass() {
+		let tasks = Array.from(document.querySelectorAll('.li--temp-task'));
+		tasks = tasks.map(function (task) {
+			return new ToDoTask(task.id, task.innerText)
+		});
+		return tasks;
 	}
-}
 
-function reassignClass(id, title, tasks, urgent, finished, i) {
-	var toDo = new ToDo(id, title, tasks, urgent, finished);
-	toDosArr.splice(i, 1, toDo);
-};
+	function createNewToDo(e) {
+		let tasks = reassignTaskClass();
+		let toDo = new ToDo(Date.now(), taskTitle.value, tasks);
+		toDosArr.push(toDo);
+		toDo.saveToStorage(toDosArr);
+		appendNewToDo(toDo);
+		taskTitle.value = "";
+		newTask.value = "";
+	};
 
-function reassignTaskClass() {
-	var tasks = Array.from(document.querySelectorAll('.li--temp-task'));
-	tasks = tasks.map(function(task) {
-		return new ToDoTask(task.id, task.innerText)
-	});
-	return tasks;
-}
-
-function createNewToDo(e) {
-	var tasks = reassignTaskClass();
-	var toDo = new ToDo(Date.now(), taskTitle.value, tasks);
-	toDosArr.push(toDo);
-	toDo.saveToStorage(toDosArr);
-	appendNewToDo(toDo);
-	taskTitle.value = "";
-	newTask.value = "";
-};
-
-function checkTaskInputValue(e) {
-	if (newTask.value === "") {
-		addNewTask.disabled = true;
-		addNewTask.classList.add('disabled');
-	} else {
-		addNewTask.disabled = false;
-		addNewTask.classList.remove('disabled');
+	function checkTaskInputValue(e) {
+		if (newTask.value === "") {
+			addNewTask.disabled = true;
+			addNewTask.addClass("disabled");
+		} else {
+			addNewTask.disabled = false;
+			addNewTask.removeClass("disabled");
+		}
 	}
-}
 
-function appendTempTask(e) {
-	e.preventDefault();
-	newTaskSection.insertAdjacentHTML('beforeend',
-		`<li class="li--task li--temp-task" id=${Date.now()}>
+	function appendTempTask(e) {
+		e.preventDefault();
+		newTaskSection.insertAdjacentHTML('beforeend',
+			`<li class="li--task li--temp-task" id=${Date.now()}>
 		<input type="image" src="images/delete.svg" class="li__delete">${newTask.value}</li>`);
-	newTask.value = "";
-}
+		newTask.value = "";
+	}
 
-function appendTask(toDo) {
-	var italic = "";
-	var returnString = "";
-	toDo.tasks.forEach(function(task) {
-		task.checked ?  italic = "checked" : italic = "";
-		returnString += `<li class="li--task ${italic}" id=${task.id}>
+	function appendTask(toDo) {
+		let italic = "";
+		let returnString = "";
+		toDo.tasks.forEach(function (task) {
+			task.checked ? italic = "checked" : italic = "";
+			returnString += `<li class="li--task ${italic}" id=${task.id}>
 		<input type="image" src=${task.checkedImg()} class="li__delete">${task.text}</li>`;
-	})
-	return returnString;
-}
+		})
+		return returnString;
+	}
 
-function appendNewToDo(toDo) {
-	var urgentSrc = toDo.urgentImg();
-	var urgentClass = toDo.urgent ? "section__tasks--urgent" : "";
-	var urgentFont = toDo.urgent ? "section__label--urgent" : "";
-	var urgentBorder = toDo.urgent ? "section__section--borders" : "";
-	taskSection.insertAdjacentHTML('afterbegin',
-		`<section class="section__tasks--status ${urgentClass}" id=${toDo.id}>
+	function appendNewToDo(toDo) {
+		const urgentSrc = toDo.urgentImg();
+		let urgentClass = toDo.urgent ? "section__tasks--urgent" : "";
+		let urgentFont = toDo.urgent ? "section__label--urgent" : "";
+		let urgentBorder = toDo.urgent ? "section__section--borders" : "";
+		taskSection.insertAdjacentHTML('afterbegin',
+			`<section class="section__tasks--status ${urgentClass}" id=${toDo.id}>
 			<section class="section__section--title ${urgentBorder}">
 				<h2 class="section__section--h2">${toDo.title}</h2>
 			</section>
@@ -161,81 +163,82 @@ function appendNewToDo(toDo) {
 			</section>
 		</section>
 	</section>`)
-}
-
-function clearToDoData (e) {
-	document.querySelector('.section__ul').innerHTML = "";
-}
-
-function persistedToDos(e) {
-	for (var i = 0; i < toDosArr.length; i++) {
-		var id = toDosArr[i].id;
-		var title = toDosArr[i].title;
-		var tasks = toDosArr[i].tasks;
-		var urgent = toDosArr[i].urgent;
-		var finished = toDosArr[i].finished;
-		var index = i;
-		reassignClass(id, title, tasks, urgent, finished, i);
 	}
-}
 
-function reinstantiateToDos() {
-	toDosArr.forEach(function(object) {
-		appendNewToDo(object);
-	});
-};
+	function clearToDoData(e) {
+		document.querySelector('.section__ul').innerHTML = "";
+	}
 
-function disableClearBtn(e) {
-	if (taskTitle.value !== "" ||
-		newTask.value !== "") {
+	function persistedToDos(e) {
+		for (let i = 0; i < toDosArr.length; i++) {
+			let id = toDosArr[i].id;
+			let title = toDosArr[i].title;
+			let tasks = toDosArr[i].tasks;
+			let urgent = toDosArr[i].urgent;
+			let finished = toDosArr[i].finished;
+			let index = i;
+			reassignClass(id, title, tasks, urgent, finished, i);
+		}
+	}
+
+	function reinstantiateToDos() {
+		toDosArr.forEach(function (object) {
+			appendNewToDo(object);
+		});
+	};
+
+	function disableClearBtn(e) {
+		if (taskTitle.value !== "" ||
+			newTask.value !== "") {
 			clearBtn.disabled = false;
-			clearBtn.classList.remove('disabled');
-	} else {
+			clearBtn.removeClass("disabled");
+		} else {
 			clearBtn.disabled = true;
-			clearBtn.classList.add('disabled');
+			clearBtn.addClass("disabled");
+		}
 	}
-}
 
-function disableSubmitBtn(e) {
-	if (taskTitle.value !== "") {
-		submitBtn.disabled = false;
-		submitBtn.classList.remove('disabled');
-	} else {
-		submitBtn.disabled = true;
-		submitBtn.classList.add('disabled');
+	function disableSubmitBtn(e) {
+		if (taskTitle.value !== "") {
+			submitBtn.disabled = false;
+			submitBtn.removeClass("disabled");
+		} else {
+			submitBtn.disabled = true;
+			submitBtn.addClass("disabled");
+		}
 	}
-}
 
-function checkedStatus(e) {
-	var taskId = e.target.closest('.li--task').id;
-	var task = ToDoTask.getById(taskId);
-	task.updateTask({checked: !task.checked});
-	e.target.src = task.checkedImg();
-	e.target.closest('li').classList.toggle('checked');
-}
+	function checkedStatus(e) {
+		let taskId = e.target.closest('.li--task').id;
+		let task = ToDoTask.getById(taskId);
+		task.updateTask({ checked: !task.checked });
+		e.target.src = task.checkedImg();
+		e.target.closest('li').classList.toggle('checked');
+	}
 
-function urgentStatus(e) {
-	var card = e.target.closest('.section__tasks--status');
-	var toDo = ToDo.getById(card.id);
-	var text = e.target.nextSibling.nextSibling;
-	var gGParent = e.target.parentNode.parentNode.parentNode;
-	var gParentSibling1 = e.target.parentNode.parentNode.previousSibling.previousSibling;
-	var gParentSibling2 = e.target.parentNode.parentNode.parentNode.firstChild.nextSibling;	
-	toDo.updateToDo({urgent: !toDo.urgent});
-	toDo.saveToStorage();
-	toDo.urgent ? gGParent.classList.add('section__tasks--urgent') : gGParent.classList.remove('section__tasks--urgent');
-	toDo.urgent ? gParentSibling1.classList.add('section__section--borders') : gParentSibling1.classList.remove('section__section--borders');
-	toDo.urgent ? gParentSibling2.classList.add('section__section--borders') : gParentSibling2.classList.remove('section__section--borders');
-	toDo.urgent ? text.classList.add('section__label--urgent') : text.classList.remove('section__label--urgent');
-	e.target.src = toDo.urgentImg();
-}
+	function urgentStatus(e) {
+		var card = e.target.closest('.section__tasks--status');
+		var toDo = ToDo.getById(card.id);
+		var text = e.target.nextSibling.nextSibling;
+		var gGParent = e.target.parentNode.parentNode.parentNode;
+		var gParentSibling1 = e.target.parentNode.parentNode.previousSibling.previousSibling;
+		var gParentSibling2 = e.target.parentNode.parentNode.parentNode.firstChild.nextSibling;
+		toDo.updateToDo({ urgent: !toDo.urgent });
+		toDo.saveToStorage();
+		toDo.urgent ? gGParent.addClass("section__tasks--urgent") : gGParent.removeClass("section__tasks--urgent");
+		toDo.urgent ? gParentSibling1.addClass("section__section--borders") : gParentSibling1.removeClass("section__section--borders");
+		toDo.urgent ? gParentSibling2.addClass("section__section--borders") : gParentSibling2.removeClass("section__section--borders");
+		toDo.urgent ? text.addClass("section__label--urgent") : text.removeClass("section__label--urgent");
+		e.target.src = toDo.urgentImg();
+	}
 
-function deleteToDoCard(e) {
-	var card = e.target.parentNode.parentNode.parentNode;
-	var toDo = ToDo.getById(card.id);
-	var newArr = toDo.tasks.filter(obj => obj.checked === true)
-	if (newArr.length === toDo.tasks.length) {
-		card.remove();
-		toDo.deleteFromStorage();
-	} 
-}
+	function deleteToDoCard(e) {
+		var card = e.target.parentNode.parentNode.parentNode;
+		var toDo = ToDo.getById(card.id);
+		var newArr = toDo.tasks.filter(obj => obj.checked === true)
+		if (newArr.length === toDo.tasks.length) {
+			card.remove();
+			toDo.deleteFromStorage();
+		}
+	}
+})
--- todo-list.js
+++ todo-list.js
@@ -1,73 +1,77 @@
-class ToDo {
-	constructor(id, title, tasks, urgent, finished) {
-		this.id 	= id;
-		this.title 	= title;
-		this.tasks 	= (tasks || []).map(function(task) {
-	  	return new ToDoTask(task.id, task.text, task.checked)
-	  });
-		this.urgent = urgent || false;
-		this.finished = finished || false;
-	}
+$(document).ready(() => {
 
-	static getById(id) {
-		return toDosArr.find(function(toDo) {
-			return toDo.id === parseInt(id);
-		})
-	}
+	class ToDo {
+		constructor(id, title, tasks, urgent, finished) {
+			this.id = id;
+			this.title = title;
+			this.tasks = (tasks || []).map(function (task) {
+				return new ToDoTask(task.id, task.text, task.checked)
+			});
+			this.urgent = urgent || false;
+			this.finished = finished || false;
+		}
 
-	saveToStorage() {
-		localStorage.setItem('toDosArr', JSON.stringify(toDosArr));
-	}
-
-	deleteFromStorage() {
-		var id = this.id;
-		var index = toDosArr.findIndex(function(toDo) {
-			return parseInt(toDo.id) === parseInt(id);
-		})
-		toDosArr.splice(index, 1);
-		this.saveToStorage();
-	};
-
-	updateToDo(obj) {
-		Object.assign(this, obj)
-		this.saveToStorage();
-	}
-
-	urgentImg() {
-		return this.urgent ? 'images/urgent-active.svg' : 'images/urgent.svg';
-	}
-}
-
-class ToDoTask {
-	constructor(id, text, checked) {
-		this.id 		= id;
-		this.text 		= text;
-		this.checked 	= checked || false;
-	}
-
-	static getById(id) {
-		var task;
-		toDosArr.forEach(function(toDo) {
-			toDo.tasks.forEach(function(t) {
-				if (parseInt(t.id) === parseInt(id)){
-					task = t;
-					return;
-				};
+		static getById(id) {
+			return toDosArr.find(function (toDo) {
+				return toDo.id === parseInt(id);
 			})
-		})
-		return task;
+		}
+
+		saveToStorage() {
+			localStorage.setItem('toDosArr', JSON.stringify(toDosArr));
+		}
+
+		deleteFromStorage() {
+			var id = this.id;
+			var index = toDosArr.findIndex(function (toDo) {
+				return parseInt(toDo.id) === parseInt(id);
+			})
+			toDosArr.splice(index, 1);
+			this.saveToStorage();
+		};
+
+		updateToDo(obj) {
+			Object.assign(this, obj)
+			this.saveToStorage();
+		}
+
+		urgentImg() {
+			return this.urgent ? 'images/urgent-active.svg' : 'images/urgent.svg';
+		}
 	}
 
-	saveToStorage() {
-		localStorage.setItem('toDosArr', JSON.stringify(toDosArr));
+	class ToDoTask {
+		constructor(id, text, checked) {
+			this.id = id;
+			this.text = text;
+			this.checked = checked || false;
+		}
+
+		static getById(id) {
+			var task;
+			toDosArr.forEach(function (toDo) {
+				toDo.tasks.forEach(function (t) {
+					if (parseInt(t.id) === parseInt(id)) {
+						task = t;
+						return;
+					};
+				})
+			})
+			return task;
+		}
+
+		saveToStorage() {
+			localStorage.setItem('toDosArr', JSON.stringify(toDosArr));
+		}
+
+		updateTask(obj) {
+			Object.assign(this, obj)
+			this.saveToStorage();
+		}
+
+		checkedImg() {
+			return this.checked ? 'images/checkbox-active.svg' : 'images/checkbox.svg';
+		}
 	}
 
-	updateTask(obj) {
-		Object.assign(this, obj)
-		this.saveToStorage();
-	}
-
-	checkedImg() {
-		return this.checked ? 'images/checkbox-active.svg' : 'images/checkbox.svg';
-	}
-}
+})
